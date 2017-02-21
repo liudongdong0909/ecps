6056fe8abd652dd089b65b8167017ecfec42c8a2
liudongdong0909 <liudongdong0909@yeah.net>
2017-02-21 16:24:39 +0800

    创建公共工具 bean 工程

---
+++ ecps-manager-model/src/main/java/com/ecps/model/TbOrder.java
@@ -0,0 +1,381 @@
+package com.ecps.model;
+
+import com.ecps.model.base.BaseModel;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Date;
+
+@ApiModel(value="订单表",description="数据库表：tb_order")
+@Table(name = "tb_order")
+public class TbOrder extends BaseModel{
+
+    @ApiModelProperty(value="订单id",name="orderId", required=true)
+    @NotEmpty
+    @Id
+    @Column(name = "order_id")
+    private String orderId;
+
+    @ApiModelProperty(value="实付金额。精确到2位小数;单位:元。如:200.07，表示:200元7分",name="payment")
+    @Column(name = "payment")
+    private String payment;
+
+    @ApiModelProperty(value="支付类型，1、在线支付，2、货到付款",name="paymentType")
+    @Column(name = "payment_type")
+    private Integer paymentType;
+
+    @ApiModelProperty(value="邮费。精确到2位小数;单位:元。如:200.07，表示:200元7分",name="postFee")
+    @Column(name = "post_fee")
+    private String postFee;
+
+    @ApiModelProperty(value="状态：1、未付款，2、已付款，3、未发货，4、已发货，5、交易成功，6、交易关闭",name="status")
+    @Column(name = "status")
+    private Integer status;
+
+    @ApiModelProperty(value="付款时间",name="paymentTime")
+    @Column(name = "payment_time")
+    private Date paymentTime;
+
+    @ApiModelProperty(value="发货时间",name="consignTime")
+    @Column(name = "consign_time")
+    private Date consignTime;
+
+    @ApiModelProperty(value="交易完成时间",name="endTime")
+    @Column(name = "end_time")
+    private Date endTime;
+
+    @ApiModelProperty(value="交易关闭时间",name="closeTime")
+    @Column(name = "close_time")
+    private Date closeTime;
+
+    @ApiModelProperty(value="物流名称",name="shippingName")
+    @Column(name = "shipping_name")
+    private String shippingName;
+
+    @ApiModelProperty(value="物流单号",name="shippingCode")
+    @Column(name = "shipping_code")
+    private String shippingCode;
+
+    @ApiModelProperty(value="用户id",name="userId")
+    @Column(name = "user_id")
+    private Long userId;
+
+    @ApiModelProperty(value="买家留言",name="buyerMessage")
+    @Column(name = "buyer_message")
+    private String buyerMessage;
+
+    @ApiModelProperty(value="买家昵称",name="buyerNick")
+    @Column(name = "buyer_nick")
+    private String buyerNick;
+
+    @ApiModelProperty(value="买家是否已经评价",name="buyerRate")
+    @Column(name = "buyer_rate")
+    private Integer buyerRate;
+
+    /**
+     * 获取 订单id 字段:tb_order.order_id
+     *
+     * @return tb_order.order_id, 订单id
+     */
+    public String getOrderId() {
+        return orderId;
+    }
+
+    /**
+     * 设置 订单id 字段:tb_order.order_id
+     *
+     * @param orderId the value for tb_order.order_id, 订单id
+     */
+    public void setOrderId(String orderId) {
+        this.orderId = orderId == null ? null : orderId.trim();
+    }
+
+    /**
+     * 获取 实付金额。精确到2位小数;单位:元。如:200.07，表示:200元7分 字段:tb_order.payment
+     *
+     * @return tb_order.payment, 实付金额。精确到2位小数;单位:元。如:200.07，表示:200元7分
+     */
+    public String getPayment() {
+        return payment;
+    }
+
+    /**
+     * 设置 实付金额。精确到2位小数;单位:元。如:200.07，表示:200元7分 字段:tb_order.payment
+     *
+     * @param payment the value for tb_order.payment, 实付金额。精确到2位小数;单位:元。如:200.07，表示:200元7分
+     */
+    public void setPayment(String payment) {
+        this.payment = payment == null ? null : payment.trim();
+    }
+
+    /**
+     * 获取 支付类型，1、在线支付，2、货到付款 字段:tb_order.payment_type
+     *
+     * @return tb_order.payment_type, 支付类型，1、在线支付，2、货到付款
+     */
+    public Integer getPaymentType() {
+        return paymentType;
+    }
+
+    /**
+     * 设置 支付类型，1、在线支付，2、货到付款 字段:tb_order.payment_type
+     *
+     * @param paymentType the value for tb_order.payment_type, 支付类型，1、在线支付，2、货到付款
+     */
+    public void setPaymentType(Integer paymentType) {
+        this.paymentType = paymentType;
+    }
+
+    /**
+     * 获取 邮费。精确到2位小数;单位:元。如:200.07，表示:200元7分 字段:tb_order.post_fee
+     *
+     * @return tb_order.post_fee, 邮费。精确到2位小数;单位:元。如:200.07，表示:200元7分
+     */
+    public String getPostFee() {
+        return postFee;
+    }
+
+    /**
+     * 设置 邮费。精确到2位小数;单位:元。如:200.07，表示:200元7分 字段:tb_order.post_fee
+     *
+     * @param postFee the value for tb_order.post_fee, 邮费。精确到2位小数;单位:元。如:200.07，表示:200元7分
+     */
+    public void setPostFee(String postFee) {
+        this.postFee = postFee == null ? null : postFee.trim();
+    }
+
+    /**
+     * 获取 状态：1、未付款，2、已付款，3、未发货，4、已发货，5、交易成功，6、交易关闭 字段:tb_order.status
+     *
+     * @return tb_order.status, 状态：1、未付款，2、已付款，3、未发货，4、已发货，5、交易成功，6、交易关闭
+     */
+    public Integer getStatus() {
+        return status;
+    }
+
+    /**
+     * 设置 状态：1、未付款，2、已付款，3、未发货，4、已发货，5、交易成功，6、交易关闭 字段:tb_order.status
+     *
+     * @param status the value for tb_order.status, 状态：1、未付款，2、已付款，3、未发货，4、已发货，5、交易成功，6、交易关闭
+     */
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    /**
+     * 获取 付款时间 字段:tb_order.payment_time
+     *
+     * @return tb_order.payment_time, 付款时间
+     */
+    public Date getPaymentTime() {
+        return paymentTime;
+    }
+
+    /**
+     * 设置 付款时间 字段:tb_order.payment_time
+     *
+     * @param paymentTime the value for tb_order.payment_time, 付款时间
+     */
+    public void setPaymentTime(Date paymentTime) {
+        this.paymentTime = paymentTime;
+    }
+
+    /**
+     * 获取 发货时间 字段:tb_order.consign_time
+     *
+     * @return tb_order.consign_time, 发货时间
+     */
+    public Date getConsignTime() {
+        return consignTime;
+    }
+
+    /**
+     * 设置 发货时间 字段:tb_order.consign_time
+     *
+     * @param consignTime the value for tb_order.consign_time, 发货时间
+     */
+    public void setConsignTime(Date consignTime) {
+        this.consignTime = consignTime;
+    }
+
+    /**
+     * 获取 交易完成时间 字段:tb_order.end_time
+     *
+     * @return tb_order.end_time, 交易完成时间
+     */
+    public Date getEndTime() {
+        return endTime;
+    }
+
+    /**
+     * 设置 交易完成时间 字段:tb_order.end_time
+     *
+     * @param endTime the value for tb_order.end_time, 交易完成时间
+     */
+    public void setEndTime(Date endTime) {
+        this.endTime = endTime;
+    }
+
+    /**
+     * 获取 交易关闭时间 字段:tb_order.close_time
+     *
+     * @return tb_order.close_time, 交易关闭时间
+     */
+    public Date getCloseTime() {
+        return closeTime;
+    }
+
+    /**
+     * 设置 交易关闭时间 字段:tb_order.close_time
+     *
+     * @param closeTime the value for tb_order.close_time, 交易关闭时间
+     */
+    public void setCloseTime(Date closeTime) {
+        this.closeTime = closeTime;
+    }
+
+    /**
+     * 获取 物流名称 字段:tb_order.shipping_name
+     *
+     * @return tb_order.shipping_name, 物流名称
+     */
+    public String getShippingName() {
+        return shippingName;
+    }
+
+    /**
+     * 设置 物流名称 字段:tb_order.shipping_name
+     *
+     * @param shippingName the value for tb_order.shipping_name, 物流名称
+     */
+    public void setShippingName(String shippingName) {
+        this.shippingName = shippingName == null ? null : shippingName.trim();
+    }
+
+    /**
+     * 获取 物流单号 字段:tb_order.shipping_code
+     *
+     * @return tb_order.shipping_code, 物流单号
+     */
+    public String getShippingCode() {
+        return shippingCode;
+    }
+
+    /**
+     * 设置 物流单号 字段:tb_order.shipping_code
+     *
+     * @param shippingCode the value for tb_order.shipping_code, 物流单号
+     */
+    public void setShippingCode(String shippingCode) {
+        this.shippingCode = shippingCode == null ? null : shippingCode.trim();
+    }
+
+    /**
+     * 获取 用户id 字段:tb_order.user_id
+     *
+     * @return tb_order.user_id, 用户id
+     */
+    public Long getUserId() {
+        return userId;
+    }
+
+    /**
+     * 设置 用户id 字段:tb_order.user_id
+     *
+     * @param userId the value for tb_order.user_id, 用户id
+     */
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    /**
+     * 获取 买家留言 字段:tb_order.buyer_message
+     *
+     * @return tb_order.buyer_message, 买家留言
+     */
+    public String getBuyerMessage() {
+        return buyerMessage;
+    }
+
+    /**
+     * 设置 买家留言 字段:tb_order.buyer_message
+     *
+     * @param buyerMessage the value for tb_order.buyer_message, 买家留言
+     */
+    public void setBuyerMessage(String buyerMessage) {
+        this.buyerMessage = buyerMessage == null ? null : buyerMessage.trim();
+    }
+
+    /**
+     * 获取 买家昵称 字段:tb_order.buyer_nick
+     *
+     * @return tb_order.buyer_nick, 买家昵称
+     */
+    public String getBuyerNick() {
+        return buyerNick;
+    }
+
+    /**
+     * 设置 买家昵称 字段:tb_order.buyer_nick
+     *
+     * @param buyerNick the value for tb_order.buyer_nick, 买家昵称
+     */
+    public void setBuyerNick(String buyerNick) {
+        this.buyerNick = buyerNick == null ? null : buyerNick.trim();
+    }
+
+    /**
+     * 获取 买家是否已经评价 字段:tb_order.buyer_rate
+     *
+     * @return tb_order.buyer_rate, 买家是否已经评价
+     */
+    public Integer getBuyerRate() {
+        return buyerRate;
+    }
+
+    /**
+     * 设置 买家是否已经评价 字段:tb_order.buyer_rate
+     *
+     * @param buyerRate the value for tb_order.buyer_rate, 买家是否已经评价
+     */
+    public void setBuyerRate(Integer buyerRate) {
+        this.buyerRate = buyerRate;
+    }
+
+    /**
+     * This method was generated by MyBatis Generator.
+     * This method corresponds to the database table tb_order
+     *
+     * @mbggenerated
+     */
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", orderId=").append(orderId);
+        sb.append(", payment=").append(payment);
+        sb.append(", paymentType=").append(paymentType);
+        sb.append(", postFee=").append(postFee);
+        sb.append(", status=").append(status);
+        sb.append(", created=").append(created);
+        sb.append(", updated=").append(updated);
+        sb.append(", paymentTime=").append(paymentTime);
+        sb.append(", consignTime=").append(consignTime);
+        sb.append(", endTime=").append(endTime);
+        sb.append(", closeTime=").append(closeTime);
+        sb.append(", shippingName=").append(shippingName);
+        sb.append(", shippingCode=").append(shippingCode);
+        sb.append(", userId=").append(userId);
+        sb.append(", buyerMessage=").append(buyerMessage);
+        sb.append(", buyerNick=").append(buyerNick);
+        sb.append(", buyerRate=").append(buyerRate);
+        sb.append("]");
+        return sb.toString();
+    }
+}
---
+++ ecps-manager-model/src/main/java/com/ecps/model/TbOrderShipping.java
@@ -0,0 +1,242 @@
+package com.ecps.model;
+
+import com.ecps.model.base.BaseModel;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@ApiModel(value="",description="数据库表：tb_order_shipping")
+@Table(name = "tb_order_shipping")
+public class TbOrderShipping extends BaseModel{
+
+    @ApiModelProperty(value="订单ID",name="orderId", required=true)
+    @NotEmpty
+    @Id
+    @Column(name = "order_id")
+    private String orderId;
+
+    @ApiModelProperty(value="收货人全名",name="receiverName")
+    @Column(name = "receiver_name")
+    private String receiverName;
+
+    @ApiModelProperty(value="固定电话",name="receiverPhone")
+    @Column(name = "receiver_phone")
+    private String receiverPhone;
+
+    @ApiModelProperty(value="移动电话",name="receiverMobile")
+    @Column(name = "receiver_mobile")
+    private String receiverMobile;
+
+    @ApiModelProperty(value="省份",name="receiverState")
+    @Column(name = "receiver_state")
+    private String receiverState;
+
+    @ApiModelProperty(value="城市",name="receiverCity")
+    @Column(name = "receiver_city")
+    private String receiverCity;
+
+    @ApiModelProperty(value="区/县",name="receiverDistrict")
+    @Column(name = "receiver_district")
+    private String receiverDistrict;
+
+    @ApiModelProperty(value="收货地址，如：xx路xx号",name="receiverAddress")
+    @Column(name = "receiver_address")
+    private String receiverAddress;
+
+    @ApiModelProperty(value="邮政编码,如：310001",name="receiverZip")
+    @Column(name = "receiver_zip")
+    private String receiverZip;
+
+    /**
+     * 获取 订单ID 字段:tb_order_shipping.order_id
+     *
+     * @return tb_order_shipping.order_id, 订单ID
+     */
+    public String getOrderId() {
+        return orderId;
+    }
+
+    /**
+     * 设置 订单ID 字段:tb_order_shipping.order_id
+     *
+     * @param orderId the value for tb_order_shipping.order_id, 订单ID
+     */
+    public void setOrderId(String orderId) {
+        this.orderId = orderId == null ? null : orderId.trim();
+    }
+
+    /**
+     * 获取 收货人全名 字段:tb_order_shipping.receiver_name
+     *
+     * @return tb_order_shipping.receiver_name, 收货人全名
+     */
+    public String getReceiverName() {
+        return receiverName;
+    }
+
+    /**
+     * 设置 收货人全名 字段:tb_order_shipping.receiver_name
+     *
+     * @param receiverName the value for tb_order_shipping.receiver_name, 收货人全名
+     */
+    public void setReceiverName(String receiverName) {
+        this.receiverName = receiverName == null ? null : receiverName.trim();
+    }
+
+    /**
+     * 获取 固定电话 字段:tb_order_shipping.receiver_phone
+     *
+     * @return tb_order_shipping.receiver_phone, 固定电话
+     */
+    public String getReceiverPhone() {
+        return receiverPhone;
+    }
+
+    /**
+     * 设置 固定电话 字段:tb_order_shipping.receiver_phone
+     *
+     * @param receiverPhone the value for tb_order_shipping.receiver_phone, 固定电话
+     */
+    public void setReceiverPhone(String receiverPhone) {
+        this.receiverPhone = receiverPhone == null ? null : receiverPhone.trim();
+    }
+
+    /**
+     * 获取 移动电话 字段:tb_order_shipping.receiver_mobile
+     *
+     * @return tb_order_shipping.receiver_mobile, 移动电话
+     */
+    public String getReceiverMobile() {
+        return receiverMobile;
+    }
+
+    /**
+     * 设置 移动电话 字段:tb_order_shipping.receiver_mobile
+     *
+     * @param receiverMobile the value for tb_order_shipping.receiver_mobile, 移动电话
+     */
+    public void setReceiverMobile(String receiverMobile) {
+        this.receiverMobile = receiverMobile == null ? null : receiverMobile.trim();
+    }
+
+    /**
+     * 获取 省份 字段:tb_order_shipping.receiver_state
+     *
+     * @return tb_order_shipping.receiver_state, 省份
+     */
+    public String getReceiverState() {
+        return receiverState;
+    }
+
+    /**
+     * 设置 省份 字段:tb_order_shipping.receiver_state
+     *
+     * @param receiverState the value for tb_order_shipping.receiver_state, 省份
+     */
+    public void setReceiverState(String receiverState) {
+        this.receiverState = receiverState == null ? null : receiverState.trim();
+    }
+
+    /**
+     * 获取 城市 字段:tb_order_shipping.receiver_city
+     *
+     * @return tb_order_shipping.receiver_city, 城市
+     */
+    public String getReceiverCity() {
+        return receiverCity;
+    }
+
+    /**
+     * 设置 城市 字段:tb_order_shipping.receiver_city
+     *
+     * @param receiverCity the value for tb_order_shipping.receiver_city, 城市
+     */
+    public void setReceiverCity(String receiverCity) {
+        this.receiverCity = receiverCity == null ? null : receiverCity.trim();
+    }
+
+    /**
+     * 获取 区/县 字段:tb_order_shipping.receiver_district
+     *
+     * @return tb_order_shipping.receiver_district, 区/县
+     */
+    public String getReceiverDistrict() {
+        return receiverDistrict;
+    }
+
+    /**
+     * 设置 区/县 字段:tb_order_shipping.receiver_district
+     *
+     * @param receiverDistrict the value for tb_order_shipping.receiver_district, 区/县
+     */
+    public void setReceiverDistrict(String receiverDistrict) {
+        this.receiverDistrict = receiverDistrict == null ? null : receiverDistrict.trim();
+    }
+
+    /**
+     * 获取 收货地址，如：xx路xx号 字段:tb_order_shipping.receiver_address
+     *
+     * @return tb_order_shipping.receiver_address, 收货地址，如：xx路xx号
+     */
+    public String getReceiverAddress() {
+        return receiverAddress;
+    }
+
+    /**
+     * 设置 收货地址，如：xx路xx号 字段:tb_order_shipping.receiver_address
+     *
+     * @param receiverAddress the value for tb_order_shipping.receiver_address, 收货地址，如：xx路xx号
+     */
+    public void setReceiverAddress(String receiverAddress) {
+        this.receiverAddress = receiverAddress == null ? null : receiverAddress.trim();
+    }
+
+    /**
+     * 获取 邮政编码,如：310001 字段:tb_order_shipping.receiver_zip
+     *
+     * @return tb_order_shipping.receiver_zip, 邮政编码,如：310001
+     */
+    public String getReceiverZip() {
+        return receiverZip;
+    }
+
+    /**
+     * 设置 邮政编码,如：310001 字段:tb_order_shipping.receiver_zip
+     *
+     * @param receiverZip the value for tb_order_shipping.receiver_zip, 邮政编码,如：310001
+     */
+    public void setReceiverZip(String receiverZip) {
+        this.receiverZip = receiverZip == null ? null : receiverZip.trim();
+    }
+
+    /**
+     * This method was generated by MyBatis Generator.
+     * This method corresponds to the database table tb_order_shipping
+     *
+     * @mbggenerated
+     */
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", orderId=").append(orderId);
+        sb.append(", receiverName=").append(receiverName);
+        sb.append(", receiverPhone=").append(receiverPhone);
+        sb.append(", receiverMobile=").append(receiverMobile);
+        sb.append(", receiverState=").append(receiverState);
+        sb.append(", receiverCity=").append(receiverCity);
+        sb.append(", receiverDistrict=").append(receiverDistrict);
+        sb.append(", receiverAddress=").append(receiverAddress);
+        sb.append(", receiverZip=").append(receiverZip);
+        sb.append(", created=").append(created);
+        sb.append(", updated=").append(updated);
+        sb.append("]");
+        return sb.toString();
+    }
+}
